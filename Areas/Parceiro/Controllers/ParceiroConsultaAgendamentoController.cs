3e5b0fc315b26eafbaea573e2a42fc9b16d120c9
stefanibispo <stefani.pessoal@hotmail.com>
2024-05-27 21:01:40 -0300

    Calendario final

---
+++ Areas/Parceiro/Controllers/ParceiroConsultaAgendamentoController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using Cartools.Models;
+using Cartools.Context;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
+
+namespace Cartools.Areas.Parceiro.Controllers
+{
+    [Area("Parceiro")]
+    [Authorize(Roles = "Parceiro")]
+    public class ParceiroConsultaAgendamentoController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<ParceiroConsultaAgendamentoController> _logger;
+
+        public ParceiroConsultaAgendamentoController(AppDbContext context, UserManager<IdentityUser> userManager, ILogger<ParceiroConsultaAgendamentoController> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Parceiro/Controllers/ParceiroDefinirDisponibilidadeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using System.Linq;
+using System.Threading.Tasks;
+using Cartools.Models;
+using Cartools.Context;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Authorization;
+using Newtonsoft.Json;
+
+namespace Cartools.Areas.Parceiro.Controllers
+{
+    [Area("Parceiro")]
+    [Authorize(Roles = "Parceiro")]
+    public class ParceiroDefinirDisponibilidadeController : Controller
+    {
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly ILogger<ParceiroDefinirDisponibilidadeController> _logger;
+
+        public ParceiroDefinirDisponibilidadeController(AppDbContext context, UserManager<IdentityUser> userManager, ILogger<ParceiroDefinirDisponibilidadeController> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            _logger.LogInformation("Entrando na controller");
+            return View();
+        }
+    }
+}
